71c0d6d7d8b834ad2ab27a22027d2d04518b0c69
Abhishek kumar Jha <Abhishekkjha068@outlook.com>
2023-12-29 02:15:52 +0530

    fileuplaod

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -47,13 +47,13 @@ const userSchema = new Schema(
     },
   },
   {
-    timestamps: true,
+    timeseries: true,
   }
 );
 
 userSchema.pre("save",async function(next){
     if(!this.isModified("password")) return next();
-    this.password =  await bcrypt.hash(this.password, 10)
+    this.password = bcrypt.hash(this.password, 10)
     next()
 })
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,22 +1,8 @@
-import { Router } from "express";
+import {Router} from "express";
 import { registerUser } from "../controllers/user.controllers.js";
-import { upload } from "../middlewares/multer.middlewares.js";
 
-const router = Router();
+const router =Router()
 
-router.route("/register").post(
-  upload.fields([
-    {
-      name: "avatar",
-      maxCount: 1,
-    },
+router.route("/register").post(registerUser)
 
-    {
-      name: "coverImage",
-      maxCount: 1,
-    },
-  ]),
-  registerUser
-);
-
-export default router;
+export default router
